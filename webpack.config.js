7f4b4d8c6e8c55b9ef268e86007c07896b8f1746
Sarah Lindmar <srhcrete@gmail.com>
2018-03-08 15:08:22 -0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src/front-end');
+
+const config = {
+   entry: {
+     main: APP_DIR + '/index.js'
+   },
+   output: {
+     filename: 'bundle.js',
+     path: BUILD_DIR,
+   },
+   module: {
+    rules: [
+     {
+       test: /\.(jsx|js)?$/,
+       use: [{
+         loader: "babel-loader",
+         options: {
+           cacheDirectory: true,
+           presets: ['react', 'es2015'] // Transpiles JSX and ES6
+         }
+       }]
+     }
+    ],
+
+  }
+};
+
+module.exports = config;
